7ad879e248f26ec54fb13a67104feed8eadd4c0e
Dmytro Firsov <dmytro_firsov@epam.com>
2023-02-12 19:01:02 +0200

    zephyr-dom0-xt: initial dom0 commit
    This commit adds very initial support for Zephyr Dom0 App.
    
    It's capable of creating exactly one domain - DomD. Domain
    config is hardcoded in domd_cfg.c file.
    
    The DomD kernel image and DTB are built-in Zephyr Dom0 App in designated linker sections
    in data region and accessible through in callbacks in domain config structure.
    
    Co-authored-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Co-authored-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Co-authored-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Co-authored-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Co-authored-by: Mykola Perehudov <Mykola_Perehudov@epam.com>
    Co-authored-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>
    [set default domu to zephyr xenvm sync sample]
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>

---
+++ src/domu_cfg.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <domain.h>
+#include <zephyr/xen/public/domctl.h>
+
+#include <string.h>
+
+extern char __img_domu0_start[];
+extern char __img_domu0_end[];
+extern char __dtb_domu0_start[];
+extern char __dtb_domu0_end[];
+
+static int load_domu_image_bytes(uint8_t *buf, size_t bufsize,
+		   uint64_t offset, void *image_info)
+{
+	ARG_UNUSED(image_info);
+
+	memcpy(buf, __img_domu0_start + offset, bufsize);
+	return 0;
+}
+
+static ssize_t get_domu_image_size(void *image_info, uint64_t *size)
+{
+	ARG_UNUSED(image_info);
+
+	*size = __img_domu0_end - __img_domu0_start;
+	return 0;
+}
+
+struct xen_domain_cfg domu0_cfg = {
+	.mem_kb = 16384,
+
+	.flags = (XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap),
+	.max_evtchns = 10,
+	.max_vcpus = 4,
+	.gnt_frames = 32,
+	.max_maptrack_frames = 1,
+
+	.gic_version =
+		IS_ENABLED(CONFIG_GIC_V3) ? XEN_DOMCTL_CONFIG_GIC_V3 : XEN_DOMCTL_CONFIG_GIC_V2,
+	.tee_type = XEN_DOMCTL_CONFIG_TEE_NONE,
+
+	.load_image_bytes = load_domu_image_bytes,
+	.get_image_size = get_domu_image_size,
+
+	.dtb_start = __dtb_domu0_start,
+	.dtb_end = __dtb_domu0_end,
+};
