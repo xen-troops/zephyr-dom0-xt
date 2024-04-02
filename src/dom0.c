2bcf0d671a12ea0763810e1b901cae8f806ab329
Mykola Kvach <mykola_kvach@epam.com>
2024-04-02 12:55:34 +0300

    app: add logic for parsing DomU configs from dts files
    Update CMakeLists.txt to reflect changes in source files.
    
    Add Kconfig options with DomU & DomD bin and dtb paths.
    
    Update domain_bins.S to include configuration paths for
    DomD and DomU binaries and device trees.
    
    Add code that get and fill DomU configs and configs for its
    devices from DTS. Add functions for creating DomUs based on
    DTS configs and search and create domain based on ID.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- src/dom0.c
+++ src/dom0.c
@@ -1,8 +1,12 @@
 /*
- * Copyright (c) 2023 EPAM Systems
- *
+ * Copyright (c) 2023-2024 EPAM Systems
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <domu_mgmt_cfg.h>
+#include <domain.h>
+
+/* note: stub for zephyr-xenlib */
+struct xen_domain_cfg domd_cfg = {};
 
 void main(void)
 {
