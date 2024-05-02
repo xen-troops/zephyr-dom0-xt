02c42c844720e8843e935ded0cf9ac673f462a7b
Grygorii Strashko <grygorii_strashko@epam.com>
2024-05-02 22:52:50 +0300

    zephyr-dom0-xt: add sample cfg and bins for rpi_5 domd(xenvm)
    Add sample configuration (rpi_5_domd.c) for simple Zephyr xenvm
    guest with RPI 5 HW support. Only real GPIO HW "/soc/gpio@7d517c00" is
    supported which is passed through to guest domain.
    
    It's used only with Zephyr xenvm Blinky application:
    
    west build -b xenvm -p always -S rpi_5_xen_domd samples/basic/blinky
    
    This patch adds pre-built binaries for domd image (zephyr_blinky.bin) and
    DTB (zephyr_blinky.dtb).
    
    Note. DTB is not required for Zephyr to run, so added for consistency.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ src/dom_cfg/rpi_5_domd.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <domain.h>
+#include <zephyr/xen/public/domctl.h>
+
+#include <string.h>
+
+static struct xen_domain_iomem domd_iomems[] = {
+	{ .first_mfn = 0x107D517, .nr_mfns = 0x1, .first_gfn = 0},
+};
+
+static char *domd_dt_passthrough[] = {
+	"/soc/gpio@7d517c00",
+};
+
+extern char __img_domu0_start[];
+extern char __img_domu0_end[];
+extern char __dtb_domu0_start[];
+extern char __dtb_domu0_end[];
+
+static int load_domd_image_bytes(uint8_t *buf, size_t bufsize,
+		   uint64_t offset, void *image_info)
+{
+	ARG_UNUSED(image_info);
+
+	memcpy(buf, __img_domu0_start + offset, bufsize);
+	return 0;
+}
+
+static ssize_t get_domd_image_size(void *image_info, uint64_t *size)
+{
+	ARG_UNUSED(image_info);
+
+	*size = __img_domu0_end - __img_domu0_start;
+	return 0;
+}
+
+static struct xen_domain_cfg domu_cfg_0 = {
+	.name = "rpi_5_domd",
+	.mem_kb = 16384,
+	.flags = (XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap),
+	.max_evtchns = 10,
+	.max_vcpus = 1,
+	.gnt_frames = 32,
+	.max_maptrack_frames = 1,
+	.gic_version = XEN_DOMCTL_CONFIG_GIC_V2,
+	.tee_type = XEN_DOMCTL_CONFIG_TEE_NONE,
+	.cmdline="",
+
+	.iomems = domd_iomems,
+	.nr_iomems = ARRAY_SIZE(domd_iomems),
+
+	.dt_passthrough = domd_dt_passthrough,
+	.nr_dt_passthrough = ARRAY_SIZE(domd_dt_passthrough),
+
+	.load_image_bytes = load_domd_image_bytes,
+	.get_image_size = get_domd_image_size,
+
+	.dtb_start = __dtb_domu0_start,
+	.dtb_end = __dtb_domu0_end,
+};
+
+struct xen_domain_cfg *domain_cfgs[] = {
+	&domu_cfg_0,
+	NULL,
+};
